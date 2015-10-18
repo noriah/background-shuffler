592ac1c3397d4e913a0c9ac42b973b2dd4c9a0fa
noriah <vix@noriah.dev>
2015-10-18 15:08:57 -0500

    Moved to plain-text config, added icon

--- background-shuffler.py
+++ background-shuffler.py
@@ -27,6 +27,7 @@ from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from threading import Timer
 import subprocess
+import json
 import signal
 from os import listdir
 import random
@@ -39,7 +40,7 @@ bg_types = ('.png', '.jpg')
 
 def file_filter(file_name):
     for i in bg_types:
-        if file_name.endswith(i):
+        if file_name.lower().endswith(i):
             return True
     return False
 
@@ -56,9 +57,9 @@ class Shuffler:
         self.bg_settings = Gio.Settings.new('org.gnome.desktop.background')
 
         if self.settings.background_folder is '':
-            self.settings.set_background_folder('/home/wolf/Pictures/Backgrounds')
+            self.settings.set_background_folder(os.path.join(os.environ['HOME'], 'Pictures'))
 
-        if self.settings.background_folder[-1] is not '/':
+        if self.settings.background_folder[-1] != "/" :
             self.settings.set_background_folder(self.settings.background_folder + '/')
 
         if self.settings.autostart:
@@ -135,6 +136,20 @@ class Shuffler:
         except OSError:
             pass
 
+    def reload_settings(self, item):
+        self.settings.load_settings()
+        self.refresh_bg_list(item)
+        if self.settings.autostart:
+            self.enable_autostart(item)
+        else:
+            self.disable_autostart(item)
+
+    def open_settings_file(self, item):
+        try:
+            subprocess.Popen(['xdg-open', self.settings.filename])
+        except OSError:
+            pass
+
     def open_preferences(self, item):
         pass
 
@@ -161,7 +176,7 @@ class Shuffler:
             starter_item = DesktopEntry(autostart_file)
             starter_item.set('Name', 'Background Shuffler applet')
             starter_item.set('Exec', 'background-shuffler.py')
-            starter_item.set('Icon', 'background-shuffler')
+            starter_item.set('Icon', 'background-shuffler-panel')
             starter_item.set('X-GNOME-Autostart-enabled', 'true')
             starter_item.write()
             self.settings.set_autostart(True)
@@ -173,6 +188,7 @@ class Shuffler:
             self.settings.set_autostart(False)
 
     def quit(self, item):
+        self.bg_timer.cancel()
         gtk.main_quit()
         sys.exit(0)
 
@@ -189,7 +205,7 @@ class Indicator:
     def setup_indicator(self):
         self.indicator = appindicator.Indicator.new(
             "background-shuffler-indicator",
-            "pamac-tray-no-update",
+            "bg-shuffler-panel",
             appindicator.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
@@ -238,10 +254,24 @@ class Indicator:
         item.show()
         menu.append(item)
 
+        item = gtk.MenuItem("Reload Settings")
+        item.connect("activate", self.main.reload_settings)
+        item.show()
+        menu.append(item)
+
+        item = gtk.MenuItem("Open Settings File")
+        item.connect("activate", self.main.open_settings_file)
+        item.show()
+        menu.append(item)
+
         # item = gtk.MenuItem("Preferences")
         # item.connect("activate", self.main.open_preferences)
         # item.show()
         # menu.append(item)
+
+        item = gtk.SeparatorMenuItem()
+        item.show()
+        menu.append(item)
 
         self.item_enable_autostart = gtk.MenuItem("Enable Autostart")
         self.item_enable_autostart.connect("activate", self.main.enable_autostart)
@@ -283,30 +313,52 @@ class Settings:
 
     def __init__(self, main):
         self.main = main
-        self.client = Gio.Settings.new('apps.background-shuffle')
-        
+        self.filename = os.path.join(os.environ['HOME'], '.bg-shuffler')
 
-        self.autostart = self.client.get_boolean("autostart")
-        self.background_folder = self.client.get_string("background-folder")
-        self.display_time = self.client.get_int("display-time")
-        self.reshow_time = self.client.get_int("time-before-reshow")
+        self.settings_data = {
+            "autostart": False,
+            "background_folder": '',
+            "display_time": 600,
+            "reshow_time": 600}
+
+        if not os.path.isfile(self.filename):
+            self.save_settings()
+        else:
+            self.load_settings()
 
     def set_autostart(self, b):
-        self.client.set_boolean("autostart", b)
+        self.settings_data["autostart"] = b
+        self.save_settings()
         self.autostart = b
 
     def set_background_folder(self, f):
-        self.client.set_string("background-folder", f)
+        self.settings_data["background_folder"] = f
+        self.save_settings()
         self.background_folder = f
 
     def set_display_time(self, t):
-        self.client.set_int("display-time", t)
+        self.settings_data["display_time"] = t
+        self.save_settings()
         self.display_time = t
 
     def set_reshow_time(self, t):
-        self.client.set_int("time-before-reshow", t)
+        self.settings_data["reshow_time"] = t
+        self.save_settings()
         self.reshow_time = t
 
+    def load_settings(self):
+        with open(self.filename) as infile:
+            self.settings_data = json.load(infile)
+
+        self.autostart = self.settings_data["autostart"]
+        self.background_folder = self.settings_data["background_folder"]
+        self.display_time = self.settings_data["display_time"]
+        self.reshow_time = self.settings_data["reshow_time"]
+
+    def save_settings(self):
+        with open(self.filename, 'w') as outfile:
+            json.dump(self.settings_data, outfile, indent = 2)
+            
     def main(self):
         gtk.main()
 
@@ -319,3 +371,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         app.quit("yup")
         sys.exit(0)
+
